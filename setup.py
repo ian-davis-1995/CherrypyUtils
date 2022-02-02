115bb856066d65e37616b74402c0b00565cdede5
Ian Davis <iandavis0527@gmail.com>
2022-02-02 18:14:35 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='cherrypy_utils',
+    packages=find_packages(include=['cherrypy_utils']),
+    version='0.1.0',
+    description='Collection of utility functions and modules for cherrypy web servers',
+    author='Me',
+    license'MIT',
+)
