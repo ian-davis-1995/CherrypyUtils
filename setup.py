6d59b8bec4cd4933a3b2488662880343c8cb3c8c
Ian Davis <iandavis0527@gmail.com>
2022-02-02 18:17:39 -0500

    Fixing setup.py missing =

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import find_packages, setup
+
 setup(
-    name='cherrypy_utils',
-    packages=find_packages(include=['cherrypy_utils']),
-    version='0.1.0',
-    description='Collection of utility functions and modules for cherrypy web servers',
-    author='Me',
-    license'MIT',
+    name="cherrypy_utils",
+    packages=find_packages(include=["cherrypy_utils"]),
+    version="0.1.0",
+    description="Collection of utility functions and modules for cherrypy web servers",
+    author="Me",
+    license="MIT",
 )
