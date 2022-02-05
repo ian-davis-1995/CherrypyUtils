34d2992dee943a4cb903c15b1f5135d929505fe2
Ian Davis <iandavis0527@gmail.com>
2022-02-04 21:21:08 -0500

    Adding BaseEventRecord to cherrypy utils

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="cherrypy_utils",
     packages=find_packages(include=["cherrypy_utils", "cherrypy_utils.login"]),
-    version="0.6.2",
+    version="1.0.0",
     description="Collection of utility functions and modules for cherrypy web servers",
     author="Me",
     license="MIT",
