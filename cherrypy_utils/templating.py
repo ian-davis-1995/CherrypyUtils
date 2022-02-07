d81e39657756ba5ac96b2527ccbd41b6abead4b0
Ian Davis <iandavis0527@gmail.com>
2022-02-07 11:11:04 -0500

    Changing templating module to require user to store instance of template environment

--- cherrypy_utils/templating.py
+++ cherrypy_utils/templating.py
@@ -1,20 +1,11 @@
-import pathlib
 import cherrypy
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-env = None
-
-
-def initialize(template_location):
-    global env
+def create_environment(template_location):
     cherrypy.log("Loading jinja template engine using filesystem location: {0}".format(template_location))
-    env = Environment(
+    return Environment(
         loader=FileSystemLoader(template_location),
         autoescape=select_autoescape(["html", "xml"]),
     )
-
-
-def get_env():
-    return env
